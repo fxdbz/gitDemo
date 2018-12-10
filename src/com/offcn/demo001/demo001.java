0642a2558998fd49cc5db68ae92335f90b446dec
fxdbz <965171973@qq.com>
2018-12-10 14:01:47 +0800

    第三次提交

--- src/com/offcn/demo001/demo001.java
+++ src/com/offcn/demo001/demo001.java
@@ -4,7 +4,7 @@ public class demo001 {
     public static void main(String[] args) {
         for(int i=1;i<10;i++){
         System.out.println("hello world");
-        System.out.println("begin");
+
         }
     }
 }
