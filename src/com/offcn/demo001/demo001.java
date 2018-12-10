046d97bdd13d6da14029e08c11b4acd93c494435
fxdbz <45745800+fxdbz@users.noreply.github.com>
2018-12-10 13:58:23 +0800

    Update demo001.java

--- src/com/offcn/demo001/demo001.java
+++ src/com/offcn/demo001/demo001.java
@@ -2,7 +2,9 @@ package com.offcn.demo001;
 
 public class demo001 {
     public static void main(String[] args) {
+        for(int i=1;i<10;i++){
         System.out.println("hello world");
         System.out.println("begin");
+        }
     }
 }
